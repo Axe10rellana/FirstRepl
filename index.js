0e92e06dc22e3677b678058c225763975cca0c2e
axel <49413624+Axe10rellana@users.noreply.github.com>
2022-04-06 16:15:18 +0000

    Create class Product and add to index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 //importaciones
 import {User} from "./User.js";
+import {Product} from "./Product.js";
 
 const redEyes = new User("Red", "Eyes");
+const product = new Product("notebook", 100000);
 console.info(redEyes);
+console.info(product);
